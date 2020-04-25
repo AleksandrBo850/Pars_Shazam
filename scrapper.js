e13262533b6a17f529d0486963dcb719404bf248
AleksandrBo850 <54817599+AleksandrBo850@users.noreply.github.com>
2020-04-25 17:40:07 +0300

    Update scrapper.js

--- scrapper.js
+++ scrapper.js
@@ -1,53 +1,80 @@
-const request = require('request-promise')
-const fs = require('fs');
-
 const MongoClient = require("mongodb").MongoClient;
-   
+const request = require('request-promise')
+const readline = require('readline');
+const fs = require('fs');
 const url = "mongodb://localhost:27017/";
 const mongoClient = new MongoClient(url, { useNewUrlParser: true });
 
-//each page in SHAZAM receives a response from the server in the form of a JSON file
-const country = 'US';
-const URL = `https://www.shazam.com/shazam/v3/en-US/UA/web/-/tracks/country-chart-${country}?pageSize=200&startFrom=0`;
-
-
-const options = {
-    method: 'GET',
-    json: true,
-    uri: URL
-}
-
-request(options)
-    .then(function (response) {
-
-        let data = response;
-        data = data['tracks'];
-        
-        mongoClient.connect(function(err, client){
-      
-            const db = client.db(`shazamdb${country}`);
-            const collection = db.collection(`topsongsin${country}`);
-            
-
-            for (var key in data) {
-
-                collection.insertOne({id: key, artist: `${data[key]['subtitle']}`, song: `${data[key]['title']}`}, function(err, result){
-                  
-                    if(err){ 
-                        return console.log(err);
-                    }
-                      
-                });
-
-            }
-            console.log("Done!");
-            client.close();
-            
-        });
-
-    })
-    .catch(function (err) {
-        console.log('ERROR');
+request(`https://www.shazam.com/shazam/v3/en-US/UA/web/-/tracks/country-chart-UA?pageSize=200&startFrom=0`, (err, response, json) => {
+    
+    if (err) { 
+        console.log('Fatal error! Try again after debugg...');
         throw err;
-    })
+    
+    }
+    if (!err ) {
 
+        var song = JSON.parse(json);
+
+        console.log('1. Default output in console');
+        console.log('2. Output in Mongo database');
+
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+          });
+
+        rl.question('Select:  ', (answer) => {
+            // TODO: Log the answer in a database
+            console.log(`You selected: ${answer}`);
+            rl.close();
+
+            if (Number(rl['history'][0]) == 1) {
+                DefaultOut(song);
+            }
+            if (Number(rl['history'][0]) == 2) {
+                MongoOut(song);
+            }
+            
+          });
+          
+    }
+    
+});
+
+
+function DefaultOut (song) {
+
+    for (var i = 0; i < 200; i++) {
+
+        console.log(`${i+1}. ${song['tracks'][i]['subtitle']}  -  ${song['tracks'][i]['title']}`);
+    }
+
+};
+
+function MongoOut (song) {
+    
+    mongoClient.connect(function(err, client){
+      
+                    const db = client.db(`database`);
+                    const collection = db.collection(`songdatabase`);
+                    
+        
+                    for (var i = 0; i < 200; i++) {
+        
+                        collection.insertOne({id: `${i+1}`, artist: `${song['tracks'][i]['subtitle']}`, song: `${song['tracks'][i]['title']}`}, function(err, result){
+                          
+                            if(err){ 
+                                return console.log(err);
+                            }
+                              
+                        });
+        
+                    }
+                    console.log("Done!");
+                    client.close();
+
+
+    });
+
+};
